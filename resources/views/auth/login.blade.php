f1ed297c9a728527ede18923a86e2e97915669b8
patthanapong <neversay.987@hotmail.com>
2017-07-08 19:15:07 +0700

    new index & template.css

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,8 @@
+@extends('master')
+@section('content')
+
+<h2>Login Form</h2>
+
+
+    
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="DataTemplate/css/Template.css">
+    <title>Document</title>
+</head>
+<body class="center">
+<div class="opacity">
+    <div class="header">
+        <h1>ProBetTH</h1>
+    </div>
+            <ul>
+                    <li><a href="/">หน้าหลัก</a></li>
+                    <li><a href="#news">คาสิโน</a></li>
+                    <li><a href="#contact">สล็อต</a></li>
+                    <li><a href="#contact">เกมส์เทเบิ้ล</a></li>
+                    <li><a href="#contact">เกมส์อื่นๆ</a></li>
+                    <li><a href="#about">โปรโมชั่น</a></li>
+                    <li style="float:right"><a href="">สมัครสมาชิก</a></li>
+                    <li style="float:right"><a href="/login">ล็อคอิน</a></li>
+                   
+                    
+            </ul>
+            
+            @yield('content')
+            
+</div> 
+
+    <div class="footer">
+            <h1>FOOTER</h1>
+    </div>
+</body>
+</html>
